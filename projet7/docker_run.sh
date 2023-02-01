7da28f1401d313559c8ffab5d47a89d4d009a912
RG <vagrant@devbox>
2023-02-01 12:57:46 +0000

    [UPD] Ajout de la commande docker run

---
+++ projet7/docker_run.sh
@@ -0,0 +1,13 @@
+# Consulter la dernière version de l'image sur nexus.
+# Puis lancer :
+docker pull <docker-registry-url>/<nom_image_geopython>
+
+# Récupérer l identifiant de l image (IMAGE ID) grâce à la commande `docker images`
+# Ici, mettons que l'id vaut : 526de.
+# Lancer la commande docker run:
+docker run -v $(pwd):/data --entrypoint '/bin/bash' 526de -c 'python /data/test.py'
+
+# Quelques explications:
+#  > -v $(pwd):/data : récupère le contenu courant et le met dans le répertoire /data du container
+#  > --entrypoint '/bin/bash' : pour la souce du shell
+#  > -c 'python /data/test.py' : pour exécution du script python.
