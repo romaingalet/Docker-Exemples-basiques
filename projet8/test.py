8b1950e9a165823eae57b0b1e1692086ce87a7e8
RG <vagrant@devbox>
2023-02-01 14:38:59 +0000

    [UPD] Identique à projet7, mais script Python testant les imports en vue de faire marcher bdf-mesh

---
+++ projet8/test.py
@@ -0,0 +1,40 @@
+
+print('Test imports')
+
+
+try:
+  import os
+  import sys
+  import shutil
+  import io
+  
+  import datetime 
+  import argparse
+
+  from pathlib import Path
+  
+except ImportError as impErr:
+  print("Erreur d'import de {}".format(impErr.args[0]))
+
+
+try:
+  import geopandas
+#  import fiona
+  import shapely
+except ImportError as impErr:
+#except Exception as e:
+  print("Erreur d'import geo de {}".format(impErr.args[0]))
+#  print("Erreur d'import geo")
+
+
+try:
+
+  import psycopg2
+  import logging
+  import traceback
+  import multiprocessing
+  import traceback
+  
+except ImportError as impErr:
+  print("Erreur d'import (autre) de {}".format(impErr.args[0]))
+
